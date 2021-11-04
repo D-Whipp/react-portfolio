e43df4bb150abf2398a5dfb67ee1ebfccbb95807
Mr Whipp <dwhipp88@gmail.com>
2021-11-03 23:40:51 -0600

    re-worked about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,50 +5,57 @@ function About() {
     <section id="about-me" className="about-me">
       <h1>Who Am I</h1>
       <div>
+        <p>Hello my name is David Whipple</p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
-          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
-          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
-          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi
-          tristique justo vel turpis sollicitudin, et tristique velit convallis.
-          In hac habitasse platea dictumst. Phasellus mattis nunc sed orci
-          consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
-          ultricies mollis. In hac habitasse platea dictumst.
+          I was first exposed to Programming in High School when I took a
+          JavaScript course. Back then I had no clue what Computer Programming
+          was. I was simply looking for an excuse to play on a computer. I knew
+          JavaScript was something special after the first day. Like many
+          others, I concluded I simply was not smart enough to become a
+          Programmer. So I got a job as a chef at my small towns local gas
+          station restaurant.
+        </p>
+        <p>I worked at that restaurant for years.</p>
+        <p>
+          One day I decided I needed to do something more with my life so I
+          started looking into college. First I attended PIMA Medical
+          Institute's Dental Assisting Program. I worked for a year as a Dental
+          Assistant and concluded it wasn't for me. Not wanting to spend the
+          better part of a decade as a Dental Assistant, as I did with my
+          previous job, I went back to school.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
-          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
-          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
-          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi
-          tristique justo vel turpis sollicitudin, et tristique velit convallis.
-          In hac habitasse platea dictumst. Phasellus mattis nunc sed orci
-          consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
-          ultricies mollis. In hac habitasse platea dictumst.
+          I attended my local community college. Where I earned an Associates in
+          Applied Sciences. I got into the Nursing Program. I was proud of
+          myself because getting into the program was'nt easy. Some people
+          waited years to get in. And I managed to obtain a seat in two months.
+          I worked hard for it.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
-          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
-          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
-          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi
-          tristique justo vel turpis sollicitudin, et tristique velit convallis.
-          In hac habitasse platea dictumst. Phasellus mattis nunc sed orci
-          consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
-          ultricies mollis. In hac habitasse platea dictumst.
+          Once in I began to succeed. My wife, who is a nurse, would talk with
+          me about the program. She had gone through it a few years prior.
+          Bonding with her over our shared experiances was fun but ...I kept
+          bringing up that old JavaScript course I took back in High School.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          bibendum turpis sed ex condimentum molestie. Mauris condimentum lectus
-          ut ornare dignissim. Mauris faucibus urna mi, ac feugiat metus aliquam
-          maximus. Proin aliquam justo nec diam vulputate vestibulum. Aenean
-          sollicitudin nulla at nisi ornare, nec suscipit massa eleifend. Morbi
-          tristique justo vel turpis sollicitudin, et tristique velit convallis.
-          In hac habitasse platea dictumst. Phasellus mattis nunc sed orci
-          consequat laoreet. Praesent id nisl nibh. Curabitur imperdiet
-          ultricies mollis. In hac habitasse platea dictumst.
+          Halfways through the Nursing Program I was struggling. Not with the
+          clinical portion. Helping others was never a problem. I couldn't
+          generate interst in the material. It quickly became apparent I was in
+          the wrong career.
         </p>
+        <p>
+          My wife pointed out how much I enjoyed talking about the JavaScript
+          course I took back in High School and she had the idea that maybe I
+          should give Programming a try. So I did. I took a few courses on
+          Udemy, Lynda.com, and after concluding how much I loved it I decided
+          to attend a Coding Boot Camp.
+        </p>
+        <p>
+          Here I am about to graduate the University of Arizona's FullStack
+          Coding Bootcamp. I couldn't be happier. Soon I will read, study, and
+          write Code for a living and I couldn't imagine a better world.
+        </p>
+        
       </div>
     </section>
   );
