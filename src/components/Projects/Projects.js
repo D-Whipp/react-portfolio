0fbeb61768f30496bae48bf8c644aec88c15b293
Mr Whipp <dwhipp88@gmail.com>
2022-04-06 00:44:14 -0600

    refactoring projects layouts

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -49,12 +49,15 @@ function Projects() {
                   <li className="project-p">Refactor</li>
                 </ul>
                 <p className="project-p">
-                  I was handed this project with the task of{" "}
-                  <b>Refactoring the code</b>.
-                </p>
-                <p className="project-p">
-                  I went through the provided code and fixed broken links, made
-                  the code <i>DRY</i>, and <i>fixed some css punctuation</i>.
+                  I was handed this project with the task <br />
+                  of <b>Refactoring the code</b>. I went <br />
+                  through the provided code and fixed broken <br />
+                  links, made the code <i>DRY</i>, and{" "}
+                  <i>
+                    fixed <br />
+                    some css punctuation
+                  </i>
+                  .
                 </p>
               </div>
             </div>
@@ -95,11 +98,9 @@ function Projects() {
                   <li className="project-p">CSS3</li>
                 </ul>
                 <p className="project-p">
-                  I was tasked with creating a static portfolio from scratch.
-                </p>
-                <p className="project-p">
-                  Using newly acquired skills in HTML5 and CSS3 I build this
-                  site from scratch.
+                  I was tasked with creating a static portfolio <br />
+                  from scratch. Using newly acquired skills in <br />
+                  HTML5 and CSS3 I build this site from scratch.
                 </p>
               </div>
             </div>
@@ -108,22 +109,46 @@ function Projects() {
           {/* Password Generator */}
           <div className="carousel-item carousel-item-three">
             <div className="slide-size-container">
-              <p className="project-title">Password Generator</p>
-              <a
-                href="https://github.com/D-Whipp/js-password-generator"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <p className="project-subtitle">Github Repository</p>
-              </a>
+              <div className="project-img-link">
+                <a
+                  href="https://d-whipp.github.io/js-password-generator/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-title">Password Generator</p>
+                  <div className="project-img-setup password_gen"></div>
+                </a>
+              </div>
 
-              <a
-                href="https://d-whipp.github.io/js-password-generator/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <div className="project-img-setup password_gen"></div>
-              </a>
+              <div className="github-repo-link">
+                <a
+                  href="https://github.com/D-Whipp/js-password-generator"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-subtitle">Github Repository</p>
+                </a>
+                <a
+                  href="https://d-whipp.github.io/js-password-generator/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-subtitle">Deployed Site</p>
+                </a>
+                <p className="project-subtitle">- Technologies Used -</p>
+                <ul>
+                  <li className="project-p">HTML5</li>
+                  <li className="project-p">CSS3</li>
+                  <li className="project-p">JavaScript</li>
+                </ul>
+                <p className="project-p">
+                  Here I was given the HTML and CSS <br />
+                  and tasked with writing the JavaScript. <br />
+                  Using Window Prompts, I wrote code that <br />
+                  will interact with the user to create a <br />
+                  random password.
+                </p>
+              </div>
             </div>
           </div>
           {/* carousel item 4 */}
@@ -133,22 +158,43 @@ function Projects() {
           {/* Work Day Scheduler */}
           <div className="carousel-item carousel-item-two">
             <div className="slide-size-container">
-              <p className="project-title">Work Day Scheduler</p>
-              <a
-                href="https://github.com/D-Whipp/work-day-scheduler"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <p className="project-subtitle">Github Repository</p>
-              </a>
+              <div className="project-img-link">
+                <a
+                  href="https://d-whipp.github.io/work-day-scheduler/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-title">Work Day Scheduler</p>
+                  <div className="project-img-setup work-day-scheduler"></div>
+                </a>
+              </div>
 
-              <a
-                href="https://d-whipp.github.io/work-day-scheduler/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <div className="project-img-setup work-day-scheduler"></div>
-              </a>
+              <div className="github-repo-link">
+                <a
+                  href="https://github.com/D-Whipp/work-day-scheduler"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-subtitle">Github Repository</p>
+                </a>
+                <a
+                  href="https://d-whipp.github.io/work-day-scheduler/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-subtitle">Deployed Site</p>
+                </a>
+                <p className="project-subtitle">- Technologies Used -</p>
+                <ul>
+                  <li className="project-p">HTML5</li>
+                  <li className="project-p">CSS3</li>
+                  <li className="project-p">JavaScript</li>
+                </ul>
+                <p className="project-p">
+                  Here I build a scheduling app to help it's user's <br />
+                  keep track of their busy day.
+                </p>
+              </div>
             </div>
           </div>
 
@@ -159,14 +205,33 @@ function Projects() {
           {/* Interactive Front-End Project | Camping Buddy */}
           <div className="carousel-item carousel-item-two">
             <div className="slide-size-container">
-              <a
-                href="https://lucawic.github.io/api-pro/"
-                rel="noreferrer"
-                target="_blank"
-              >
-                <p className="project-title">Camping Buddy</p>
-                <div className="project-img-setup camping_buddy_image"></div>
-              </a>
+              <div className="project-img-link">
+                <a
+                  href="https://lucawic.github.io/api-pro/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-title">Camping Buddy</p>
+                  <div className="project-img-setup camping_buddy_image"></div>
+                </a>
+              </div>
+
+              <div className="github-repo-link">
+                <p className="project-subtitle">No Repository</p>
+                <a
+                  href="https://lucawic.github.io/api-pro/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  <p className="project-subtitle">Deployed Site</p>
+                </a>
+                <ul>
+                  <li className="project-p">HMTL5</li>
+                  <li className="project-p">CSS3</li>
+                  <li className="project-p">JavaScript</li>
+                  <li className="project-p">Third Party API</li>
+                </ul>
+              </div>
             </div>
           </div>
 
