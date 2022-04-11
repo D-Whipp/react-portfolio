e3081f78859de6c14451dbd72258e9d3da7870ef
Mr Whipp <dwhipp88@gmail.com>
2022-04-11 15:36:24 -0600

    refactored src/components/Header/Header.js | resized images and placed into new directory | deleted project-images and the images therein

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="header-wrapper">
       <div className="main-info">
-        <h1>Fullstack Web Developer</h1>
+        <h1>Web Developer</h1>
         <Typed
           className="typed-text"
           strings={[
